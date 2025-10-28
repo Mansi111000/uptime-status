7eb601b8ad8a3839de2e5d0ec25afe410bf329d0
Mansi111000 <mansithakkar008@gmail.com>
2025-10-28 18:23:46 +0530

    Dashboard completed

--- services/status-web/pages/_app.tsx
+++ services/status-web/pages/_app.tsx
@@ -1,7 +1,5 @@
-// services/status-web/pages/_app.tsx
 import type { AppProps } from 'next/app';
-import '../styles/globals.css'; // <-- load Tailwind
-
+import '../styles/globals.css';
 export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
--- services/status-web/tailwind.config.js
+++ services/status-web/tailwind.config.js
@@ -1,20 +1,5 @@
-// // services/status-web/tailwind.config.js
-// /** @type {import('tailwindcss').Config} */
-// module.exports = {
-//   darkMode: ["class"],
-//   content: [
-//     "./pages/**/*.{js,ts,jsx,tsx}",
-//     "./components/**/*.{js,ts,jsx,tsx}",
-//     "./app/**/*.{js,ts,jsx,tsx}",
-//   ],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-// };
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class', // or 'media'
+  darkMode: 'class',
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
